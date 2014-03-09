5a1ff1ad465a33a813072fe3d323c54fca174843
Baptiste MORIN <morin.baptiste@gmail.com>
2014-03-09 18:20:28 +0100

    fix Status values

--- src/main/java/org/bm/files/rules/Statuses.java
+++ src/main/java/org/bm/files/rules/Statuses.java
@@ -8,10 +8,9 @@ import org.bm.rules.Status;
  * @author Baptiste Morin
  */
 public enum Statuses {
-    OK(new DefaultFileStatus(Integer.MAX_VALUE, "OK", null)),
-    INVALID_OPTIONAL_HEADER(new DefaultFileStatus(100_000, "Optional header is invalid", "Check the first line of the file.")),
-
-    INVALID_LINE(new DefaultFileStatus(90_000, "The line is invalid, it can not be read by the application", "Check the line.")),;
+    OK(new DefaultFileStatus(Integer.MIN_VALUE, "OK", null)),
+    INVALID_OPTIONAL_HEADER(new DefaultFileStatus(-8_000, "Optional header is invalid", "Check the first line of the file.")),
+    INVALID_LINE(new DefaultFileStatus(0, "The line is invalid, it can not be read by the application", "Check the line.")),;
 
 
     private final Status status;
